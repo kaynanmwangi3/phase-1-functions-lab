fa8db82019fd8c3559698e1fa953f46f8318da33
Kaynan Mwangi <kaynanmwangi6@gmail.com>
2025-06-10 22:21:28 +0300

    updates iindex.html

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+// Code your solution in this file
+function distanceFromHqInBlocks(block) {
+    if (block < 42) {
+        return 42 - block;
+    } else {
+        return block - 42;
+    }
+}
+function distanceFromHqInFeet(block) {
+    const blockDistance = distanceFromHqInBlocks(block);
+    return blockDistance * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+        return (destination - start) * 264;
+    } else {
+        return (start - destination) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    
+    if (distance > 2500) {
+        return "cannot travel that far";
+    } else if (distance > 2000) {
+        return 25;
+    } else if (distance > 400) {
+        return (distance - 400) * 0.02;
+    } else {
+        return 0;
+    }
+}
